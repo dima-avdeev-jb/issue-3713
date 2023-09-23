86b33856b016ea79e2b5b91de27ba1d7362b094d
dima.avdeev <dima.avdeev@jetbrains.com>
2023-09-23 11:23:30 +0400

    Can't reproduce

--- desktopApp/src/jvmMain/kotlin/main.kt
+++ desktopApp/src/jvmMain/kotlin/main.kt
@@ -1,8 +1,22 @@
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         MainView()
+        IconButton(
+            onClick = {
+            }
+        ) {
+            Icon(
+                painter = painterResource("ic_refresh.svg"),
+                contentDescription = "Refresh"
+            )
+
+        }
     }
 }
